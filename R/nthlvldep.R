61b2a9b357b34e304d8fa89980c09e0dabdbd141
falo0 <o.menssen@me.com>
2018-08-25 23:17:15 +0200

    nthlvldep has a lot more functionality now. plotdstr makes use of this and is otherwise like plot_depgraph2.R

--- R/nthlvldep.R
+++ R/nthlvldep.R
@@ -4,31 +4,55 @@
 #' dependencies. It outputs the dependencies, dependencies of dependencies, ...
 #' of these first level dependencies. There are various options for the type of
 #' the output.
+#' @param githublink A link to a github repository of an R package
 #' @param pkg A list of packages from which we want to know the further
-#' dependencies
+#' dependencies. This list will be added to the first level dependencies
+#' of a given package on github it githublink is set
 #' @param outtype Possible output types:
-#' edge_list: An edge list, e.g. to be used for network plots
-#' allPkgs: An overview of all packages that are eventually loaded. No further
+#' edgelist: An edge list, e.g. to be used for network plots
+#' edgelistdetailed: An edge list, e.g. to be used for network plots, including
+#' information on the type of dependency
+#' allpackages: An overview of all packages that are eventually loaded. No further
 #' structure visible
 #' list: More detailed than allPKs, it's a list tha containns all packages per
 #' first level dependency
-#' unique_list: like "list", just excluding all packages, that are eventually
+#' uniquelist: like "list", just excluding all packages, that are eventually
 #' also loaded by another package in firstlvldep. This way you can see which
 #' dependencies will be removed completely if you remove a certain first level
 #' dependency (a package that you import).
 #' tree: Detailed information about which package depends on which, represented
 #' in a data frame that is showing a tree structure.
+#' firstlvlpkgs: The vector of packages used as input. This is a combination
+#' when both the githublink and the pkg parameter are set.
+#' rootpackage: Whether to include the name of the package of the given
+#' github link. Returns "Root Package" when no github link was given
+#' @param includebasepkgs Whether to include base packages in the analysis or not
+#' @param recursive Whether you want to look deeper than the second level of dependencies,
+#' e.g. get all dependencies of dependencies of dependencies ...
+#' @param which What type of depencencies are regarded
 #' @export
-#' @importFrom utils available.packages installed.packages
+#' @importFrom utils available.packages installed.packages read.csv
 
 
-nthlvldep <- function(pkg, outtype = "edge_list"){
+nthlvldep <- function(githublink = NULL, pkg = NULL, outtype,
+                      includebasepkgs = F, recursive = T,
+                      which = c("Imports", "Depends")){
 
-  if(!is.null(pkg)){
-    stopifnot(is.character(pkg))
+  if(is.null(pkg) & is.null(githublink)){
+    stop("You have to specify at least one of the two: pkg, githublink")
+  } else if (!is.null(githublink) & is.null(pkg)){
+    # only a github link and no vector of packages was given
+    res <- firstlvldep(githublink, includeRootPkg = T)
+    rootPkgName <- unname(res[[1]])
+    pkg <- res[[2]]
+  } else if (!is.null(githublink) & !is.null(pkg)){
+    # both a githublink and a vector of packges where given, e.g. to test
+    # what would happen if a certain package would also import the given vector
+    # of packages
+    pkg <- unique(c(firstlvldep(githublink), pkg))
   }
 
-  #find the base packages
+  #read the list of base packages
   base.pkgs <- rownames(installed.packages(priority="base"))
 
   #if pkg not assigned use packages activated
@@ -39,86 +63,146 @@ nthlvldep <- function(pkg, outtype = "edge_list"){
 
   bigmat <- available.packages()  #have to check if Git Hub repository
   deplevels <- c("Imports","Depends")
+  #recursive <- T
+  #which <- deplevels
+  #includebasepkgs <- F
 
-  edge.list <- function(pkg, recursive = T,
-                        which = c("Imports","Depends"), includebasepkgs = F){
-
-
-    #create a vector of all needed packages (the vertices)
-    all.pkgs <- tools::package_dependencies(pkg, recursive = recursive, which = c("Imports","Depends"), db=bigmat)
-    all.pkgs <- as.vector(unique(unlist(lapply(all.pkgs,rbind))))
-    if (length(all.pkgs)==0){
-      if (length(pkg)>1){
-        stop("Your packages have no further dependencies")
-      } else {
-        stop("Your package has no further dependencies")
-      }
+  #create a vector of all needed packages (the vertices)
+  all.pkgs <- tools::package_dependencies(pkg, recursive = recursive, which = deplevels, db=bigmat)
+  all.pkgs <- as.vector(unique(unlist(lapply(all.pkgs,rbind))))
+  if (length(all.pkgs)==0){
+    if (length(pkg)>1){
+      stop("Your packages have no further dependencies")
+    } else {
+      stop("Your package has no further dependencies")
     }
-    all.pkgs <- c(pkg,all.pkgs)
+  }
+  all.pkgs <- c(pkg,all.pkgs)
 
 
-    #create a function which takes a pkgname as input and returns a dataframe with the
-    #depending firstlevel packages in column 2 (end.vertex). The 3rd column "dependencies"
-    #is an additional information for the edgelist, and shows the type of the connection.
+  #create a function which takes a pkgname as input and returns a dataframe with the
+  #depending firstlevel packages in column 2 (end.vertex). The 3rd column "dependencies"
+  #is an additional information for the edgelist, and shows the type of the connection.
 
-    level_pkg <- function(x){
-      results <- data.frame("start" = character(),"end" = character())
+  level_pkg <- function(x){
+    results <- data.frame("start" = character(),"end" = character(), "dependency" = character())
+
+    for (i in 1:length(deplevels)){
+      results <- data.frame("start" = character(),"end" = character(), "dependency" = character())
 
       for (i in 1:length(deplevels)){
-        pkgs <- unlist(tools::package_dependencies(x, recursive = F , which = deplevels[i], db=bigmat))
+        pkgs <-   unlist(tools::package_dependencies(x, recursive = F , which = deplevels[i], db=bigmat))
 
         results <- rbind(results, data.frame("start" = rep(x, length(unlist(pkgs))),
-                                             "end" =  c(unlist(pkgs))))
+                                             "end" =  c(unlist(pkgs)),
+                                             "dependencies" = rep(deplevels[i],length(unlist(pkgs)))))
       }
       return(results)
     }
-
-    # combine the small dataframes for each package to one edgelist
-    if (recursive == T){
-      result.df <-do.call(rbind,lapply(all.pkgs, level_pkg))
-    } else {
-
-      result.df <-do.call(rbind,lapply(pkg, level_pkg))
-
-    }
-
-
-    #if necessary delete all connections to base packages
-    if ((includebasepkgs == F)) {
-      result.df <- subset(result.df, !(result.df[, 1] %in% base.pkgs))
-      result.df <- subset(result.df, !(result.df[, 2] %in% base.pkgs))
-      all.pkgs <- subset(all.pkgs, !(all.pkgs %in% base.pkgs))
-    }
-
-    rownames(result.df) <- NULL
-
-    #return a list of the edgelist in form of a dataframe and the vector of all
-    #packages needed (this is maybe not necessary and can be deleted)
-    return(list(result.df, all.pkgs))
   }
-  result <- edge.list(pkg)
+
+
+  # combine the small dataframes for each package to one edgelist
+  if (recursive == T){
+    result.df <-do.call(rbind,lapply(all.pkgs, level_pkg))
+  } else {
+
+    result.df <-do.call(rbind,lapply(pkg, level_pkg))
+
+  }
+  row.names(result.df) <- NULL
+
+  #if necessary delete all connections to base packages
+  if ((includebasepkgs == F)) {
+    result.df <- subset(result.df, !(result.df[, 1] %in% base.pkgs))
+    result.df <- subset(result.df, !(result.df[, 2] %in% base.pkgs))
+    all.pkgs <- subset(all.pkgs, !(all.pkgs %in% base.pkgs))
+  }
 
   if(length(outtype) == 1){
-    if(outtype == "edge_list"){
-      return(result[[1]])
-    } else if (outtype == "allPkgs"){
-      return(result[[2]])
+    if(outtype == "edgelist"){
+      return(result.df[,-3])
+    } else if (outtype == "edgelistdetailed"){
+      return(result.df)
+    } else if (outtype == "allpackages"){
+      return(all.pkgs)
+    } else if (outtype == "list"){
+      return(tools::package_dependencies(pkg, recursive = T , which = deplevels, db=bigmat))
+    } else if (outtype == "uniquelist"){
+      list <- tools::package_dependencies(pkg, recursive = T , which = deplevels, db=bigmat)
+      uniquelist <- list()
+      for(i in 1:length(list)){
+        everythingelse <- unname(unlist(list[-i]))
+        uniquelist[[i]] <- setdiff(list[[i]], everythingelse)
+      }
+      names(uniquelist) <- pkg
+      return(uniquelist)
+    } else if (outtype == "tree"){
+      treeDF <- edges2tree(result.df[,-3], lvl1deps = pkg)
+      # Optionally replace NA by "" so it is nicer to look at. Not sure if we should keep this
+      treeDF[is.na(treeDF)] <- ""
+      return(treeDF)
+    } else if (outtype == "firstlvlpkgs"){
+      return(pkg)
+    } else if (outtype == "rootpackage"){
+      if(exists("rootPkgName")){
+        return(rootPkgName)
+      } else {
+        return("Root Package")
+      }
     } else {
       stop("outtype has to be at least on of the following (possibly a vector of
-           several): edge_list, allPkgs")
+           several): edgelist, edgelistdetailed, allpackages, list,
+           uniquelist, tree, firstlvlpkgs, rootpackage")
     }
 
-  } else if (length(outtype) == 1){
+
+
+  } else if (length(outtype) > 1){
+
+    #outtype <- c("edgelist", "allpackges")
+
     outlist <- list()
-    if(outtype == "edge_list"){
-      outlist[[length(outlist)+1]] <- result[[1]]
-    } else if (outtype == "allPkgs"){
-      outlist[[length(outlist)+1]] <- result[[2]]
-    } else {
-      stop("outtype has to be at least on of the following (possibly a vector of
-           several): edge_list, allPkgs")
+    for (i in 1:length(outtype)){
+      if(outtype[i] == "edgelist"){
+        outlist[[length(outlist)+1]] <- result.df[,-3]
+      } else if (outtype[i] == "edgelistdetailed"){
+        outlist[[length(outlist)+1]] <- result.df
+      } else if (outtype[i] == "allpackages"){
+        outlist[[length(outlist)+1]] <- all.pkgs
+      } else if (outtype[i] == "list"){
+        outlist[[length(outlist)+1]] <- tools::package_dependencies(pkg,
+                                  recursive = T , which = deplevels, db=bigmat)
+      } else if (outtype[i] == "uniquelist"){
+        list <- tools::package_dependencies(pkg, recursive = T , which = deplevels, db=bigmat)
+        uniquelist <- list()
+        for(i in 1:length(list)){
+          everythingelse <- unname(unlist(list[-i]))
+          uniquelist[[i]] <- setdiff(list[[i]], everythingelse)
+        }
+        names(uniquelist) <- pkg
+        outlist[[length(outlist)+1]] <- uniquelist
+      } else if (outtype[i] == "tree"){
+        treeDF <- edges2tree(result.df[,-3], lvl1deps = pkg)
+        # Optionally replace NA by "" so it is nicer to look at. Not sure if we should keep this
+        treeDF[is.na(treeDF)] <- ""
+        outlist[[length(outlist)+1]] <- treeDF
+      } else if (outtype[i] == "firstlvlpkgs"){
+        outlist[[length(outlist)+1]] <- pkg
+      } else if (outtype[i] == "rootpackage"){
+        if(exists("rootPkgName")){
+          outlist[[length(outlist)+1]] <-rootPkgName
+        } else {
+          outlist[[length(outlist)+1]] <-"Root Package"
+        }
+      } else {
+        stop("outtype has to be at least one of the following (possibly a vector of
+             several): edgelist, edgelistdetailed, allpackages, list,
+             uniquelist, tree, firstlvlpkgs, rootpackage")
+      }
     }
 
+    names(outlist) <- outtype
     return(outlist)
 
   } else {
---
+++ R/plotdstr.R
@@ -0,0 +1,102 @@
+#' Plot Dependencs Structure
+#'
+#' This function plots the dependency structur of a package
+#' @param githublink A link to a github repository of an R package
+#' @param pkg A list of packages from which we want to know the further
+#' dependencies. This list will be added to the first level dependencies
+#' of a given package on github it githublink is set
+#' @param includebasepkgs Whether to include base packages in the analysis or not
+#' @param recursive Whether you want to look deeper than the second level of dependencies,
+#' e.g. get all dependencies of dependencies of dependencies ...
+#' @param which What type of depencencies are regarded
+#' @import igraph
+#' @export
+#' @importFrom graphics mtext plot
+
+plotdstr <- function(githublink= NULL, pkg=NULL, includebasepkgs = F, recursive = T,
+                     which = c("Imports", "Depends")){
+  #pkg <- frst
+  #recursive <- T
+  #includebasePkgs <- F
+  #deplevels <- c("Imports", "Depends")
+  #githublink <- "tidyverse/ggplot2"
+  #githublink <- NULL
+  bigmat <- available.packages()
+
+  #Create the edgelists
+  data <- nthlvldep(githublink = githublink, pkg = pkg, recursive = recursive,
+                     which = which,
+                     includebasepkgs = includebasepkgs,
+                     outtype = c("edgelistdetailed","allpackages",
+                                 "firstlvlpkgs", "rootpackage"))
+
+  all.data <- data[[1]]
+  names(all.edges) <- c("start.vertex", "end.vertex", "dependencies")
+  all.vertices <- data[[2]]
+  firstlvl_vertices <- data[[3]]
+  github_pkg <- data[[4]]
+
+  if (nrow(all.edges) == 0) {
+    stop(("There are no dependencies"))
+    # Feature request: Is it possible to only draw the packages in "pkg"
+    # (which are not connected to anything of course) and then give this warning?
+    # warning("There are no dependencies")
+    # This is probably the behavior the user expects instead of an error message
+    # denying to plot anything
+  }
+
+  #identify first level dependencies
+  firstlvl <- all.edges$start.vertex %in% pkg
+  all.edges$firstlevel <- firstlvl
+
+  # Ich glaube daruf geht der Code weiter unten schon ein, bin mir nicht ganz sicher:
+  # Es kann auch first level packages geben, die selbst keine depencendies haben
+  # Diese sollten auch im netplot erscheinen (die schweben dann halt alleine
+  # ohne irgend eine Verbindung herum. Dass keine weiteren Dependencies da sind,
+  # ist ja auch eine Information)
+
+  #create network
+  # Achtung! Im treeDF kann man gut erkennen, wie es wirklich sein kann, dass
+  # z.b. Bei GREA zweimal shiny in den allPkgs ist, weil das halt von verschiedenen
+  # packages an verschiedenen Levels geladen wird. K.p. ob in solchen Fällen
+  # der netplot anders aussehen müsste. Als Quickfix habe ich erstmal unique
+  # genommen
+  uniqueVertices <- unique(all.vertices) # <- DAUERLÖSUNG??
+
+
+  net <- graph_from_data_frame(d=all.edges, vertices = uniqueVertices, directed = T)
+
+
+  l <- layout_with_fr(net) * 2 #define the network algortihm to the "Fruchterman Reingold"-Algorithm
+
+  #color vertices depending on input, firstlvl or other
+  V(net)$vcolor <- ifelse(V(net)$name %in% pkg,"steelblue", ifelse(V(net)$name %in% firstlvl_vertices, "orange","black"))
+
+  V(net)$label.cex <- 1
+  V(net)$label.cex[which(V(net)$name %in% pkg)] <- 1.2
+
+  if (!is.null(githublink)){
+    V(net)$label.cex[which(V(net)$name %in% github_pkg)] <- 1.2
+    V(net)$vcolor[which(V(net)$name %in% github_pkg)] <- "red"
+  }
+
+  #check if there are unidentified packages
+  lonely.vertices <- V(net)$name[degree(net, mode="all")==0]
+  if (length(lonely.vertices >  0)){
+    for (i in  1:length(lonely.vertices)){
+      if(!(lonely.vertices[i] %in% bigmat[,1])){
+        warning(paste0("Package ",lonely.vertices[i]," can't be found in repository. Are you sure you entered the packagename correctly?"))
+      }
+    }
+  }
+  plot(net, edge.arrow.size = .1, edge.color="darkgrey",vertex.size = 10, vertex.shape = "none",
+       vertex.frame.color = "white", vertex.label.font= 1, vertex.label.color = V(net)$vcolor,
+       edge.width = 1.5 , main = "Dependency Graph",
+       frame = T, edge.curved = curve_multiple(net,start = 1) #falls wir noch suggests hinzuf?gen und es doppelte pfeile von v1 zu v2 gibt
+       , layout = l, vertex.label.cex = V(net)$label.cex)
+  mtext(paste("Number of packages: ",length(all.vertices)), side=1, line=0, adj=1, cex=0.75)
+}
+
+#plotdstr(pkg = frst, recursive = T, includebasePkgs = F)
+#plotdstr(pkg = c("miniCRAN", "ggplot2"), recursive = T, includebasePkgs = F)
+#plotdstr("Stan125/GREA")
