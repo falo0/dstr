2c4fcd0437ad9b2f0a734af0b5acdcf590ae4aab
falo0 <o.menssen@me.com>
2018-08-29 15:49:03 +0200

    Ran devtools::check(), wich automatically updated the .Rd files so that no which parameter is included in the documentation anymore. check() gave a note that dstr.network() still inputs the which paramter into the nthlvldep(), so I just removed that.

--- R/dstr.network.R
+++ R/dstr.network.R
@@ -1,14 +1,14 @@
 # a wrapper function which creates an igraph network object from input packages.
 # The network object can be used for plotting a user defined network graph
-dstr.network <- function(githublink = NULL, pkg = NULL,includebasepkgs = F, recursive = T,which = c("Imports", "Depends")) {
-  
+dstr.network <- function(githublink = NULL, pkg = NULL,includebasepkgs = F, recursive = T) {
+
   #create edgelist
-  edgelist <- nthlvldep(githublink = githublink, pkg = pkg, outtype="edgelistdetailed", includebasepkgs = includebasepkgs, recursive = recursive, which= which)
-  
-  
-  
+  edgelist <- nthlvldep(githublink = githublink, pkg = pkg, outtype="edgelistdetailed", includebasepkgs = includebasepkgs, recursive = recursive)
+
+
+
   network <- igraph::graph_from_data_frame(d=edgelist, directed = T)
-  
+
   return(network)
 }
 
