7cc549c91bfe1ddcf1ed2c8e6ad75d0afaf3c7c3
albers93 <chrisalbers11@web.de>
2018-09-01 00:05:24 +0200

    Wrote some test for nthlvldep. I also checked if outputs are correctly delivered.

---
+++ tests/testthat/test_nthlvldep.R
@@ -0,0 +1,59 @@
+context("does nthlvldep perform right?")
+
+library("dstr")
+library("testthat")
+
+result <- nthlvldep(pkg="dplyr", recursive=T, includebasepkgs = F, outtype="all_packages")
+result2 <- nthlvldep(pkg="dplyr", recursive=T, includebasepkgs = F, outtype="tree")
+result3 <- nthlvldep(pkg="dplyr", recursive=T, includebasepkgs = F, outtype="edgelist")
+result4 <- nthlvldep(pkg="dplyr", recursive=F, includebasepkgs = F, outtype="all_packages")
+result5 <- nthlvldep(pkg="dplyr", recursive=F, includebasepkgs = F, outtype="list")
+
+base_pkgs <- rownames(installed.packages(priority="base"))
+
+test_that("nthlvldep returns no base_pkgs when includebasepkgs=F",{
+
+  #function which checks for any base pkgs
+  check_base <- function(x){
+    any(base_pkgs %in% x)
+  }
+
+  expect_false(check_base(result))
+  expect_false(any(apply(result2, 2, check_base) ==T))
+  expect_false(any(apply(result3, 2, check_base) ==T))
+
+})
+
+
+test_that("recursive = T is working", {
+  expect_true(length(result) > length(result4))
+  })
+
+
+test_that("output is equal to tools package_dependencies()",{
+
+  true_deps <- as.vector(unlist(tools::package_dependencies("dplyr", db=bigmat, which=c("Depends","Imports"),
+                                          recursive=T)))
+ true_deps <- true_deps[which(!true_deps %in% base_pkgs)]
+
+ expect_true(length(true_deps) == (length(result)-1))
+ expect_true(all(true_deps %in% result))
+  })
+
+
+test_that("output is of correct type",{
+
+  expect_true(class(result) == "character")
+  expect_true(class(result2) == "data.frame")
+  expect_true(class(result3) == "data.frame")
+  expect_true(class(result5) == "list")
+
+})
+
+
+rm(result)
+rm(result2)
+rm(result3)
+rm(result4)
+rm(result5)
+rm(base_pkgs)
